d9732c6836ebefe6807cde0f991ccca09d108d2d
Alexis Montes <montes.alexis25@gmail.com>
2021-01-12 18:50:52 -0500

    finished initial version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+const marked = require("marked");
+
+marked.setOptions({
+  renderer: new marked.Renderer(),
+  pedantic: false,
+  gfm: true,
+  breaks: false,
+  sanitize: false,
+  smartLists: true,
+  smartypants: false,
+  xhtml: true
+})
 
 function App() {
+  const [textAreaValue, setTextAreaValue] = React.useState("")
+
+  let handleTextAreaChange = e => {
+    setTextAreaValue(e.target.value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container">
+      <div>
+        <p className="h1">Markdown Previewer</p>
+      </div>
+      <div className="row ">
+        <div className="col-md-6 form-group">
+          <textarea 
+            id="editor"
+            className="form-control"
+            rows="12"
+            value={textAreaValue}
+            onChange={handleTextAreaChange}
+          />
+        </div>
+        <div className="col-md-6">
+          <div id="preview-container" className="container">
+            <p id="preview" className="text-break">
+              {marked(textAreaValue)}
+            </p>
+          </div>
+          
+        </div>
+      </div>
     </div>
   );
 }
